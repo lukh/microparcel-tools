e7dfa36cbb0ef02d8bd590dbd3ec5edb0bd650e0
Vivien Henry <vivien.henry@outlook.fr>
2019-03-06 18:49:42 +0100

    using sub cat

--- microparcel_tools/generators/cxx_router_h.py
+++ microparcel_tools/generators/cxx_router_h.py
@@ -7,7 +7,9 @@ template = """\
 class {{ protocol.name }}{{ sender }}Router {
     public:
         {% for leaf in protocol.root_node.leafs(sender) %}
-        {{ protocol.name }}Msg make{{ leaf.name }}(\
+{# ##############WITHOUT SUBCAT #}
+{% if not leaf.subcat %}
+        static {{ protocol.name }}Msg make{{ leaf.name }}(\
 {# METHOD ARGS#}
 {%- for fp in leaf.fields -%}\
 {%- if fp.enum -%} {{ protocol.name }}Msg::{%- endif -%}{{ fp.field_type }} in_{{ fp.name|lower }}\
@@ -23,7 +25,38 @@ class {{ protocol.name }}{{ sender }}Router {
             {% for fp in leaf.fields %}
             msg.set{{ fp.name }}(in_{{ fp.name|lower }});
             {% endfor %}
+
+            return msg;
         }
+{# ##############WITH SUBCAT #}
+        {% else %}
+        {% for subcat in leaf.subcat.enum.enumerators %}
+        static {{ protocol.name }}Msg make{{ leaf.name }}{{subcat}}(\
+{# METHOD ARGS#}
+{%- for fp in leaf.fields -%}\
+{%- if fp.enum -%} {{ protocol.name }}Msg::{%- endif -%}{{ fp.field_type }} in_{{ fp.name|lower }}\
+{% if not loop.last %}, {% endif %}{%- endfor -%}){
+\
+            {{ protocol.name }}Msg msg = {{ protocol.name }}Msg();
+
+            {% for pleaf in leaf.parents() %}
+            {# UGLY, SHOULD USE ENUM... #}
+            msg.set{{ pleaf.parent.name }}({{ protocol.name }}Msg::{{ pleaf.parent.name }}_{{pleaf.name}}); 
+            {% endfor %}
+
+            {# UGLY, SHOULD USE ENUM... #}
+            msg.set{{ leaf.name}}({{ protocol.name }}Msg::{{ leaf.name}}_{{ subcat }});
+
+            {% for fp in leaf.fields %}
+            msg.set{{ fp.name }}(in_{{ fp.name|lower }});
+            {% endfor %}
+
+            return msg;
+        }
+
+        {% endfor %}
+
+        {% endif %} 
 
         {% endfor %}
 
