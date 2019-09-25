f54f6f1ce6896f7cf8c885e81a143134867730c1
Vivien Henry <vivien.henry@outlook.fr>
2019-09-25 08:55:33 +0200

    docs doesn't need req

--- microparcel_tools/cli.py
+++ microparcel_tools/cli.py
@@ -14,6 +14,7 @@ from .microparcel_tools import Protocol
 
 from .generators import message, router
 
+
 def main(schema_file, cxx=None, py=None):
     """Generate serialization/deserialization code from schema"""
     logging.info('Working on file: ' + schema_file)
@@ -28,14 +29,12 @@ def main(schema_file, cxx=None, py=None):
     with open(schema_file) as fd:
         schema = json_parser.load(fd)
 
-
     # validating the schema
     status, msg = validate_protocol_schema(schema)
     if not status:
         logging.info('Invalid Schema:' + msg)
         return -2
     logging.info('Schema is valid')
-
 
     # Build the protocol
     protocol = Protocol(schema)
@@ -66,8 +65,10 @@ def main(schema_file, cxx=None, py=None):
 
 
 def cli():
-    parser = argparse.ArgumentParser(description='microparcel parser and message generation tool')
-    parser.add_argument('schema_file', type=str, help='schema file, in json format')
+    parser = argparse.ArgumentParser(
+        description='microparcel parser and message generation tool')
+    parser.add_argument(
+        'schema_file', type=str, help='schema file, in json format')
     parser.add_argument('--cxx', help='C++ output folder')
     parser.add_argument('--py', help='C++ output folder')
 
