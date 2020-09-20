ef201481ecc3e1649bd54ddfd29bd6cd9564bb09
Vivien Henry <vivien.henry@inductivebrain.fr>
2020-09-20 12:01:10 +0200

    support for 3.7/3.8 only

--- setup.py
+++ setup.py
@@ -31,10 +31,8 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ],
     description="Code generation tool for microparcel",
     entry_points={
